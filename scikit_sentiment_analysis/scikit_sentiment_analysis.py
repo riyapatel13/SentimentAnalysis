5a8d5adad66ed8d69d53533041ef894cd7295cf9
Riya Patel <riaupatel13@yahoo.com>
2020-08-13 23:10:51 -0400

    wrote helper for svm

--- scikit_sentiment_analysis/scikit_sentiment_analysis.py
+++ scikit_sentiment_analysis/scikit_sentiment_analysis.py
@@ -28,8 +28,9 @@ def writeFile(path, contents):
         f.write(contents)
 
 
-file_path = "../data/train.tsv"
-source = open(file_path, 'r').readlines()
+training_data_path = "../data/train.tsv"
+
+source = open(training_data_path, 'r').readlines()
 # get rid of title
 source = source[1:]
 
@@ -47,8 +48,8 @@ for line in source:
     X_train.append(line_elts[0])
     Y_train.append(line_elts[1])
 
-file_path = "../data/test.tsv"
-source = open(file_path, 'r').readlines()
+test_data_path = "../data/test.tsv"
+source = open(test_data_path, 'r').readlines()
 # get rid of title
 source = source[1:]
 
@@ -76,7 +77,6 @@ X_train_counts = count_vect.fit_transform(X_train) # potentially problematic
 tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
 X_train_tf = tf_transformer.transform(X_train_counts)
 
-# perform gradient descent
 
 #support vector machine
 # setting up classifier
@@ -143,6 +143,95 @@ for doc, category in zip(X_test, predicted):
 new_file.close()
 '''
 
+'''
+Given the path to the Rotten Tomatoes movie review training dataset, the function
+will return the training data in 2 lists, where X_train is the list of sentences
+and Y_train is the sentiment assigned to those sentences. For any index i, 
+the sentence X_train[i] has the sentiment Y_train[i].
+'''
+def import_training_data(file_path = "../data/test.tsv"):
+    source = open(file_path, 'r').readlines()
+    # get rid of title
+    source = source[1:]
+
+    X_train = []
+    Y_train = []
+
+    for line in source:
+        # puts phrase id, sentence id, sentence, assignment in list
+        line_elts = line.split('\t')[2:]
+        # gets rid of newline in last element (assignment)
+        line_elts[1] = int(line_elts[1][:-1])
+        X_train.append(line_elts[0])
+        Y_train.append(line_elts[1])
+    
+    return X_train, Y_train
+
+'''
+Given the path to the Rotten Tomatoes movie review test dataset, the function
+will return the test data in a list (a list of sentences).
+'''
+def import_test_data(file_path = "../data/test.tsv"):
+    source = open(file_path, 'r').readlines()
+    # get rid of title
+    source = source[1:]
+
+    X_test = []
+
+    for line in source:
+        # puts phrase id, sentence id, sentence, assignment in list
+        line_elts = line.split('\t')[2:]
+        # gets rid of newline in last element (assignment)
+        X_test.append(line_elts[0][:-1]) 
+
+    return X_test
+
+'''
+Performs tokenization of sentences. This function builds a dictionary of features
+and transforms each sentence in the training data into a feature vector and saves
+the results into X_train_counts.
+'''
+def vectorize_sentences(X_train):
+    count_vect = CountVectorizer()
+    # potentially what is causing the poor prediction because the sentences might not be vectorizing well since they are complex
+    X_train_counts = count_vect.fit_transform(X_train) 
+    return X_train_counts
+
+def train_svm():
+    X_train, Y_train = import_training_data(training_data_path)
+    X_test = import_test_data(test_data_path)
+    X_train_counts = vectorize_sentences(X_train)
+
+    # downscale weights for words that occur in many documents in the corpus and are therefore less informative
+    tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+    X_train_tf = tf_transformer.transform(X_train_counts)
+
+    # setting up classifier and fitting
+    clf = svm.SVC(gamma=0.1, C=300)
+    clf.fit(X_train_tf,Y_train)
+
+    # prediction
+    predicted = clf.predict(X_test)
+
+    # writing to file - return X_test, predicted. Take in file name
+    svm_res = open("sentiment_analysis_results/driving_results_svm.tsv", "w")
+    for doc, category in zip(X_test, predicted):
+        svm_res.write('%r => %s\n' % (doc, predicted[category]))
+    svm_res.close()
+ 
+
+def train_nb():
+    X_train, Y_train = import_training_data(training_data_path)
+    X_test = import_test_data(test_data_path)
+    X_train_counts = vectorize_sentences(X_train)
+    pass
+
+def train_rand_forest():
+    X_train, Y_train = import_training_data(training_data_path)
+    X_test = import_test_data(test_data_path)
+    X_train_counts = vectorize_sentences(X_train)
+    pass
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description = "Scikit Sentiment Analysis - Given an input file and the algorithm type, this file will predict the sentiment of the sentences in the input file by training the data and using the algorithm given. The data it trains on is the train.tsv (data from Rotten Tomato movie reviews). It will classify each output on a scale of 0 to 4, where 0 => Negative, 1 => Somewhat Negative, 2 => Neutral, 3 => Somewhat Positive, and 4 => Positive. The results will be saved in the output file name provided.")
     argparser.add_argument("input_file",
@@ -165,4 +254,3 @@ if __name__ == "__main__":
         pass
     else:
         print("Please re-run with one of these algorithms: nb (Multi-nomial Naive Bayes), svm (Support Vector Classifier), or rf (Random Forest)")
-        return
